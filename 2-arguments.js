2a481b75a5632511ebb23315ce5ef4b191ffe6cf
RamseyToo <ramseykiprotichtoo@gmail.com>
2025-09-19 21:01:50 +0300

    Add files via upload

---
+++ 2-arguments.js
@@ -0,0 +1,11 @@
+const args = process.argv.slice(2);
+if (args.length===0){
+    console.log("no arguments");
+
+}else if (args.length===1){
+    ( "argument found");
+
+}else{
+    console.log("arguments found" );
+    
+}
